e0fb87a5e88f374d0e3a8fa1cd068d55db2cbf02
Suyash <suyash.chavan9@gmail.com>
2018-12-18 22:55:34 +0530

    base: di added to presenter

--- app/src/main/java/test/billeasy/com/billeasytest/base/BasePresenter.kt
+++ app/src/main/java/test/billeasy/com/billeasytest/base/BasePresenter.kt
@@ -1,9 +1,21 @@
 package test.billeasy.com.billeasytest.base
 
+import test.billeasy.com.billeasytest.injection.component.DaggerPresenterInjector
+import test.billeasy.com.billeasytest.injection.component.PresenterInjector
+import test.billeasy.com.billeasytest.injection.module.ContextModule
+import test.billeasy.com.billeasytest.injection.module.NetworkModule
+
 /**
  * abstract BasePresenter class will create dagger dependency injector and inject to the classes inheriting BasePresenter
  */
 abstract class BasePresenter<out V: BaseView>(protected val view: V) {
+
+    private val injector: PresenterInjector = DaggerPresenterInjector
+        .builder()
+        .baseView(view)
+        .contextModule(ContextModule)
+        .networkModule(NetworkModule)
+        .build()
 
     init {
         inject()
