730a53c4c098dbd2e3019f84dfe86de9eb62dcc6
Suyash <suyash.chavan9@gmail.com>
2018-12-19 19:03:05 +0530

    fix: git repository model fixed

--- app/src/main/java/test/billeasy/com/billeasytest/data/model/GitRepository.kt
+++ app/src/main/java/test/billeasy/com/billeasytest/data/model/GitRepository.kt
@@ -2,7 +2,12 @@ package test.billeasy.com.billeasytest.data.model
 
 data class GitRepository (
     val id : Int,
-    val avatar_url : String,
     val name : String,
+    val owner : Owner,
     val full_name : String
+)
+
+data class Owner (
+    val login : String,
+    val avatar_url : String
 )
