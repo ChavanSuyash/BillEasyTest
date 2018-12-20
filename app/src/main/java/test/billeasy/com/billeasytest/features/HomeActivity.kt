f3f004c53a2903b1ecad0694e04249a44f8d2561
Suyash <suyash.chavan9@gmail.com>
2018-12-20 12:28:30 +0530

    ui: navgation architecture component initialized

--- app/src/main/java/test/billeasy/com/billeasytest/features/HomeActivity.kt
+++ app/src/main/java/test/billeasy/com/billeasytest/features/HomeActivity.kt
@@ -1,14 +1,14 @@
 package test.billeasy.com.billeasytest.features
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import test.billeasy.com.billeasytest.R
 
 class HomeActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("Start","Here")
+        setContentView(R.layout.activity_home)
     }
 
 }
